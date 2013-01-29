b30ca2b368afa7f299771cde6460551fa04629cd
annahaus <annahaus100@gmail.com>
2013-01-29 13:29:07 -0800

    update to wk2 exercise
    php file only see santarosa.edu/~ahausfel/dw2/index.php for online page

--- lesson-02/index.php
+++ lesson-02/index.php
@@ -32,37 +32,77 @@
 <li>Call US</li>
 <li>707-762-8721</li>
 </ul>
-</div>
+</div><!--end of sidebar1 div-->
 
 <div id="content1">
+
 <h1>Introduction</h1>
 <p>I am taking Advanced Dreamweaver to learn how to create interaction web pages for business. Even if I don't do all the development I need to appreciate what is involved. Flexible, responsive, interactive, and dynamic. I am looking forward to learning more.
 </p>
 
+<h3>Simple PHP Examples</h3>
 
 <?php
-$age = ('1995-03-09');
-if ($age > 21) {
+$txt1='Hello all.';
+$txt2='This is my first PHP script!';
+echo $txt1 . ' ' . $txt2;
+?>
+
+<p>Today is</p>
+
+<?php
+echo ( date('l, F dS Y.') );
+?>
+
+
+<?php
+$t=date('H');
+if ($t<'10')
+{
+echo "Have a good morning.";
+}
+else if ($t<'20')
+{
+	echo "Have a good day.";
+}
+else
+{
+	echo "Have a good night.";
+}
+?>
+
+<p>I am</p>
+<?php
+$decades=50;
+$years=7;
+$age=$decades+$years;
+echo $age;
+?>
+
+<h4>Can I offer you a drink?</h4>
+
+<?php
+$age2=('1955-03-09');
+if ($age2>21) {
 	?>
-    <h2 style="color:green">Congratulations! You are of age. Please drink responsibly.</h2>
+    <h2>Congratulations! You are of age. Please drink responsibly.</h2>
     <?php
-    } 
+	}
 else {
 	?>
-    <h2 style="color:red">You are too young. How about a coke?</h2>
+    <h2 style="color:red">You are not yet of age. How about a coke?</h2>;
     <?php
     }
 ?>
 
-
-</div>
-</div> 
+</div><!--end of content1 div-->
+</div><!--end of container div--> 
 
 <footer>
   <p  id="copyright">&copy; 2013 Anna Hausfeld. Developed with valid <a href="http://validator.w3.org/check?uri=referer" title="HTML checker">HTML</a> and <a href="http://jigsaw.w3.org/css-validator/check/referer" title="CSS checker">CSS</a>.</p>
-  </footer>
+</footer>
 
-</div>
+</div><!--end of wrapper div-->
 
 </body>
 </html>
